6ea5b86cd4359f8a0d6dbabdfaf3c870befaa3b4
Matheus H. de Souza <mh.matheussouza@gmail.com>
2019-08-28 15:43:07 -0300

    Criado view padrão

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>{{ config('app.name') }}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    </head>
+    <body>
+        <div id="app"></div>
+
+        <script src="{{ assert('js/app.js') }}"></script>
+    </body>
+</html>
