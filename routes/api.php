ec06c46ccc7724e536324410ac2c94c33aff5bc5
Matheus H. de Souza <mh.matheussouza@gmail.com>
2019-08-29 21:47:37 -0300

    Criado rota para buscar usuários

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('/users', function () {
+    return factory('App\User', 10)->make();
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
