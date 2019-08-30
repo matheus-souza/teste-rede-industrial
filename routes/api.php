5bc9d45d0f6435108b0053f3395919365489f29d
Matheus H. de Souza <mh.matheussouza@gmail.com>
2019-08-30 18:01:18 -0300

    Criado rota para exclusão de usuário

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ Route::namespace('Api')->group(function () {
     Route::get('/users', 'UsersController@index');
     Route::get('/users/{user}', 'UsersController@show');
     Route::put('/users/{user}', 'UsersController@update');
+    Route::delete('/users/{user}', 'UsersController@destroy');
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
